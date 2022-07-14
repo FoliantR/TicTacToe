6ab688b5192337149d639682a03214278726ee10
FoliantR <s.nowgorodsky@yandex.ru>
2022-07-14 16:47:28 +0300

    v.2

--- Client/TicTacToeClient/TicTacToeClient/obj/Debug/netcoreapp3.1/App.g.cs
+++ Client/TicTacToeClient/TicTacToeClient/obj/Debug/netcoreapp3.1/App.g.cs
@@ -47,7 +47,7 @@ namespace TicTacToeClient {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace TicTacToeClient {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             TicTacToeClient.App app = new TicTacToeClient.App();
             app.InitializeComponent();
